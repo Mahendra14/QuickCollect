224e564c55200ae696013aaeb885a57597de30f6
Raichur Mahendra Kumar <51109700+Mahendra14@users.noreply.github.com>
2026-05-31 21:24:10 -0700

    [add]- adding a sepearate manifest fiel

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'data-entry-v1';
+const CACHE_NAME = 'data-entry-v2';
 const ASSETS = [
   './',
   './index.html',
+  './manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.8.2/jspdf.plugin.autotable.min.js'
 ];
